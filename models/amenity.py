f2b78c2c5278d4b2eed959676aab5e4a195e46a2
OG-1534 <gershomochanda@gmail.com>
2024-02-09 12:25:57 -0800

    class Amenity

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that inherits class Amenity from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class to import from BaseModel"""
+
+    name = ""
